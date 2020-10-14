d8bc58634aed4d3deb453ccff8ed2ea07a7a13be
AkeemDrake <60721003+AkeemDrake@users.noreply.github.com>
2020-10-13 22:07:23 -0400

    Add files via upload

---
+++ python-api-challenge/README.md
@@ -0,0 +1,2 @@
+# BUT-IND-DATA-PT-02-2020-U-C
+
---
+++ python-api-challenge/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "8baf21465052cb15ea6fa976670558ec"
+
+# Google API Key
+g_key = "AIzaSyBV47iSNUjYPNQZE2UoJyUddUSKECu6ja8"
